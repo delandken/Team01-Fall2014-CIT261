9e589ab8ee94b9ce4e86483e095b2179ed148337
Brent Smith <bsmith0213@gmail.com>
2014-12-02 18:25:34 -0700

    options view and controller

--- app/scripts/controllers/OptionsCtrl.js
+++ app/scripts/controllers/OptionsCtrl.js
@@ -1,5 +1,42 @@
 angular.module('MatchingGame').controller('OptionsCtrl', function($scope) {
-	$scope.clickMe = function() {
-		alert('test');
+	
+	// Sound toggle
+	$scope.soundOn = function() {
+		alert('Turn sound on?');
 	};
+	$scope.soundOff = function() {
+		alert('Turn sound off?');
+	};
+	
+	// Mode selection functions
+	$scope.modeEasy = function() {
+		alert('Easy Mode');
+	};
+	$scope.modeHard = function() {
+		alert('Hard Mode');
+	};
+	
+	// Game table size selection
+	$scope.sizeSmall = function() {
+		alert('4x4 Size Selected');
+	};
+	$scope.sizeMedium = function() {
+		alert('6x6 Size Selected');
+	};
+	$scope.sizeLarge = function() {
+		alert('10x10 Size Selected');
+	};
+	
+	// Card function selection
+	$scope.cardColor = function() {
+		alert('Color Cards Selected');
+	};
+	$scope.cardSimple = function() {
+		alert('Simple Cards Selected');
+	};
+	$scope.cardEnhanced = function() {
+		alert('Enhanced Cards Selected');
+	};
+	
+	
 });
