51e6097c849c9350c663342272f0d5dd658fe867
Brent Smith <bsmith0213@gmail.com>
2014-12-04 21:36:50 -0700

    high scores ctrl and view

--- app/scripts/controllers/HighScoresCtrl.js
+++ app/scripts/controllers/HighScoresCtrl.js
@@ -1,3 +1,14 @@
-angular.module('MatchingGame').controller('HighScoresCtrl', function($scope) {
+angular.module('MatchingGame').controller('HighScoresCtrl', function($scope, HighScoreService) {
+
+	
+	
+	$scope.highScores = function() {
+		//Add the high score to the list:
+		HighScoreService.add($scope.game.currentMatches).then(function(){
+			HighScoreService.getAllSortedByHighScore(5).then(function(highScores){
+				$scope.highScores = highScores;
+			});
+		});
+	}
 	
 });
